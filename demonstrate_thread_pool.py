87a69cd52225be5c1b3a0f3154caf713291fae82
tockards <toufeeq.ockards+gitlab@gmail.com>
2019-09-15 21:04:11 +0200

    add command line options for sleep_time, workers, port

--- demonstrate_thread_pool.py
+++ demonstrate_thread_pool.py
@@ -4,9 +4,14 @@ from concurrent.futures import ThreadPoolExecutor
 from tornado.concurrent import run_on_executor
 from time import sleep
 import tornado.gen
+from tornado.options import define, options
+
+define("sleep_time", default=10, help="seconds to block each request")
+define("workers", default=5, help="Number of max_workers to set for ThreadPoolExecutor")
+define("port", default=8830, help="Port to expose server on")
 
 class MainHandler(tornado.web.RequestHandler):
-    executor = ThreadPoolExecutor(max_workers=10)
+    executor = ThreadPoolExecutor(max_workers=options.workers)
 
     @run_on_executor
     def sleep_sleep(self, sleep_time):
@@ -16,7 +21,7 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         self.write("hellao")
-        sleep_time = yield self.sleep_sleep(10)
+        sleep_time = yield self.sleep_sleep(options.sleep_time)
         self.write("Hello, world {}".format(sleep_time))
 
 def make_app():
@@ -25,6 +30,7 @@ def make_app():
     ])
 
 if __name__ == "__main__":
+    tornado.options.parse_command_line()
     app = make_app()
-    app.listen(8888)
+    app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
